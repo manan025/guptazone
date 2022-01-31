4187a3be58ed44db6e5182a56542d7c996ab286d
Manan <mananman23@gmail.com>
2022-01-31 21:41:12 +0530

    customer

--- js/customerDashboard.js
+++ js/customerDashboard.js
@@ -1,2 +1,11 @@
 let name = document.getElementById("user_name");
-name.innerText = supabase.auth.currentUser.firstName;
+// name.innerText = supabase.auth.currentUser.user_metadata.firstName;
+// display each character of name one by one
+let i = 0;
+let name_interval = setInterval(function() {
+    name.innerText += supabase.auth.currentUser.user_metadata.firstName[i];
+    i++;
+    if (i >= supabase.auth.currentUser.user_metadata.firstName.length) {
+        clearInterval(name_interval);
+    }
+}, 200);
